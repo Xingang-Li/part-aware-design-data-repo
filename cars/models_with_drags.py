98cc4ba55bd16af451390a88ffc0094ab58487dd
Xingang-Li <xingang.li@utexas.edu>
2023-04-14 16:32:05 -0500

    update

---
+++ cars/models_with_drags.py
@@ -0,0 +1,60 @@
+from scipy.io import loadmat
+from pathlib import Path
+import csv
+
+#car models
+this_folder = Path(__file__).resolve().parent
+recon_folder = this_folder / "carVAE_64_128"
+SPVAE_mat_file =  recon_folder / "recover_sym.mat"
+drag_file = this_folder / "car_drags.csv"
+
+# Load the mat file
+SPVAE_data = loadmat(SPVAE_mat_file)
+# print(data)
+
+SPVAE_keys = SPVAE_data.keys()
+print(SPVAE_keys)
+name_list1 = [element.strip() for element in SPVAE_data['model_names']]
+# print(name_list1)
+# print(len(name_list)) #1162
+
+name_list2 = []
+drag_list = []
+# Open the CSV file
+with open(str(drag_file), newline='') as csvfile:
+    # Create a CSV reader object
+    reader = csv.reader(csvfile)
+    # Iterate over the rows
+    for row in reader:
+        # Output the second column
+        name = row[1].strip()
+        drag = row[2].strip()
+        if '_aug' not in name and '_flip' not in name:
+            name_list2.append(name)
+            drag_list.append(drag)
+
+overlapped_name_list = []
+overlapped_drag_list = []
+for name in name_list1:
+    if name in name_list2:
+        overlapped_name_list.append(name)
+        #index of name in name_list2
+        index = name_list2.index(name)
+        drag = drag_list[index]
+        overlapped_drag_list.append(drag)
+
+#store the names of overlapped models
+with open (this_folder / "overlapped_name_list.txt", "w") as f:
+    for name in overlapped_name_list:
+        f.write(name + "\n")  
+
+#csv file contains model names and corresponding drag coefficients
+with open(this_folder / "overlapped_names_drags.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["number", "model_name", "drag_coefficient"])
+    for i in range(len(overlapped_name_list)):
+        writer.writerow([i, overlapped_name_list[i], overlapped_drag_list[i]])
+
+
+
+
---
+++ cars/read_mat_files.py
@@ -0,0 +1,72 @@
+from scipy.io import loadmat
+from pathlib import Path
+import pandas as pd
+
+def extract_vectors(vector_file, output_file, model_drag_file):
+    # Load the vector file
+    vector_data = loadmat(SPVAE_mat_file)
+    keys = vector_data.keys()
+    print(keys) #dict_keys(['__header__', '__version__', '__globals__', 'model_names', 'emb'])
+    name_list = [element for element in vector_data['model_names']]
+    vector_list = [vector for vector in vector_data['emb']]
+
+    # Load the model drag file in csv format
+    model_drag_data = pd.read_csv(model_drag_file)
+    #show the header 
+    print(model_drag_data.head())
+    #access the column of the model name
+    model_name_list = model_drag_data['model_name']
+    drag_list = model_drag_data['drag_coefficient']
+    
+    #store i, name, vector, and drag in a csv file where vector is np.array and drag is float  
+    #create a new csv file
+    csv_file = output_file
+    csv_file.touch()
+    #write the header
+    with open(csv_file, 'w') as f:  
+        #write the data   
+        i = 0
+        f.write('i,name,')
+        for j in range(len(vector_list[0])):
+                f.write(f'dim_{j+1},')
+        f.write('drag\n')
+        
+        for name in model_name_list:
+            if name in name_list: 
+                index = name_list.index(name)    
+                vector = vector_list[index]
+                drag = drag_list[i]
+                f.write(f'{i},{name},')
+                #vector is an np array and needs to be stored one value by one value
+                for j in range(len(vector)):
+                    f.write(f'{vector[j]},')
+                f.write(f'{round(drag, 3)}\n')
+                i += 1
+    
+
+# body_mat_file = recon_folder / "body" / "recover.mat"
+# body_data = loadmat(body_mat_file)
+# part_keys = body_data.keys()
+# print(part_keys) #dict_keys(['__header__', '__version__', '__globals__', 'model_names', 'emb'])
+# body_list = [vector for vector in body_data['emb']]
+# print(len(body_list[0]))
+
+# # plane models
+# import mat73
+# plane_feature_file = this_folder / "plane_vaefeature.mat"
+# plane_feature_data = mat73.loadmat(plane_feature_file)
+# plane_feature_keys = plane_feature_data.keys()
+# print(plane_feature_keys)
+# model_list = [name for name in plane_feature_data['modelname']]
+# print(len(model_list))
+
+
+if __name__ == "__main__":
+    #car models
+    this_folder = Path(__file__).resolve().parent
+    recon_folder = this_folder / "carVAE_64_128"
+    SPVAE_mat_file =  recon_folder / "recover_sym.mat"
+    models_with_drags_file = this_folder / "overlapped_names_drags.csv"
+    spvae_file = this_folder / "spvae_vectors_drags.csv"
+    extract_vectors(spvae_file, SPVAE_mat_file, models_with_drags_file)
+    
