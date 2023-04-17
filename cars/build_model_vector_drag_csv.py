8a1939e8915eb9b35c23293306781f68ca60f790
Xingang-Li <xingang.li@utexas.edu>
2023-04-17 12:25:23 -0500

    update

--- cars/build_model_vector_drag_csv.py
+++ cars/build_model_vector_drag_csv.py
@@ -1,13 +1,60 @@
 from scipy.io import loadmat
 from pathlib import Path
 import pandas as pd
+import os
 
-def extract_vectors(vector_file, output_file, model_drag_file):
+'''
+Form csv files: model names, vectors (e.g., SPVAE vectors, part-VAE vectors, etc.), drags 
+
+Output examples: spvae_vectors_drags.csv
+'''
+
+#extract part vectors from mat files and concatenate them to store them in one csv file
+def extract_part_vectors(folder, output_file=None, category='car'):
+    if category == 'car':
+        parts = ['body', 'left_mirror', 'right_mirror', 'left_front_wheel', 
+                 'right_front_wheel', 'left_back_wheel',  'right_back_wheel']
+    k = 0
+    for part in parts[:1]:
+        subfolder_path = os.path.join(folder, part)
+        for file in os.listdir(subfolder_path):
+            file_path = os.path.join(subfolder_path, file)
+            if file_path.endswith(".mat"):
+                mat_data = loadmat(file_path)
+                # keys = mat_data.keys()
+                name_list = [element.strip() for element in mat_data['model_names']] #model names in my training set
+                vector_list = [vector for vector in mat_data['emb']]
+
+                #store i, name, vector, and drag in a csv file where vector is np.array and drag is float  
+                #create a new csv file
+                csv_file = output_file
+                csv_file.touch()
+                #write the header
+                with open(csv_file, 'a') as f:  
+                    #write the data   
+                    i = 0
+                    f.write('i,name,')
+                    for j in range(len(vector_list[0])):
+                            f.write(f'dim_{j+k*len(vector_list[0])},')
+                    
+                    for name in name_list:
+                        index = name_list.index(name)    
+                        vector = vector_list[index]
+                        f.write(f'{i},{name},')
+                        #vector is an np array and needs to be stored one value by one value
+                        for l in range(len(vector)):
+                            f.write(f'{vector[l]},')
+                        i += 1
+        k += 1
+
+
+
+def form_model_vector_drag(vector_file, model_drag_file, output_file):
     # Load the vector file
     vector_data = loadmat(vector_file)
     keys = vector_data.keys()
     print(keys) #dict_keys(['__header__', '__version__', '__globals__', 'model_names', 'emb'])
-    name_list = [element for element in vector_data['model_names']]
+    name_list = [element.strip() for element in vector_data['model_names']] #model names in my training set
     vector_list = [vector for vector in vector_data['emb']]
 
     # Load the model drag file in csv format
@@ -43,29 +90,26 @@ def extract_vectors(vector_file, output_file, model_drag_file):
                 f.write(f'{round(drag, 3)}\n')
                 i += 1
     
-# body_mat_file = recon_folder / "body" / "recover.mat"
-# body_data = loadmat(body_mat_file)
-# part_keys = body_data.keys()
-# print(part_keys) #dict_keys(['__header__', '__version__', '__globals__', 'model_names', 'emb'])
-# body_list = [vector for vector in body_data['emb']]
-# print(len(body_list[0]))
-
-# # plane models
-# import mat73
-# plane_feature_file = this_folder / "plane_vaefeature.mat"
-# plane_feature_data = mat73.loadmat(plane_feature_file)
-# plane_feature_keys = plane_feature_data.keys()
-# print(plane_feature_keys)
-# model_list = [name for name in plane_feature_data['modelname']]
-# print(len(model_list))
-
 
 if __name__ == "__main__":
+
     #car models
     this_folder = Path(__file__).resolve().parent
     recon_folder = this_folder / "carVAE_64_128"
-    SPVAE_mat_file =  recon_folder / "recover_sym.mat"
     models_with_drags_file = this_folder / "overlapped_names_drags.csv"
-    spvae_file = this_folder / "spvae_vectors_drags.csv"
-    extract_vectors(SPVAE_mat_file, spvae_file, models_with_drags_file)
+
+    # #SPVAE vectors
+    # SPVAE_mat_file =  recon_folder / "recover_sym.mat"
+    # spvae_file = this_folder / "model_vector_drag" / "spvae_vectors_drags.csv"
+    # form_model_vector_drag(vector_file=SPVAE_mat_file, model_drag_file=models_with_drags_file, output_file=spvae_file)
+
+    # #part-VAE vectors
+    # body_mat_file = recon_folder / "body" / "recover.mat"
+    # body_file = this_folder / "model_vector_drag" / "body_vectors_drags.csv"
+    # form_model_vector_drag(vector_file=body_mat_file, model_drag_file=models_with_drags_file, output_file=body_file)
+
+    #
+    parts_vectors_csv = this_folder / "model_vector_drag" / "all_parts_vectors_drags.csv"
+    extract_part_vectors(recon_folder, parts_vectors_csv, category='car')
+
     
